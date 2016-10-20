910d09ba5db8d59a6e05bd8f6ea9768505a8a49c
GideonExists <watcher.of.shadows@gmail.com>
2016-10-20 14:38:53 -0400

    Update Authority.sh

--- Authority.sh
+++ Authority.sh
@@ -3,9 +3,10 @@
 #Script to hasten set up of tor directory authorities
 
 read -p "We need to get the dependencies for TOR (y,n) " REPLY
-echo "Installing necessary dependencies..."
+
 if [ "${REPLY,,}" == "y" ]; then
 	
+	echo "Installing necessary dependencies..."	
 	apt-get --force-yes install libevent-dev
 	apt-get --force-yes install libssl-dev
 
@@ -33,13 +34,15 @@ if [ "${REPLY,,}" == "y" ]; then
 	./configure && make && make install
 	mkdir /var/lib/tor
 	touch /usr/local/etc/tor
-  mkdir /var/lib/tor/keys
+  	mkdir /var/lib/tor/keys
+	rm /usr/local/etc/tor/torrc
 fi
 
 #Set network as testing network and data directory
 echo "TestingTorNetwork 1" >> /usr/local/etc/tor/torrc
 echo "DataDirectory /var/lib/tor" >> /usr/local/etc/tor/torrc
 echo "ConnLimit 60" >> /usr/local/etc/tor/torrc
+
 #Customizing torrc to suit relay
 
 #Nickname for Relay
@@ -57,27 +60,36 @@ echo "DisableDebuggerAttachment 0" >> /usr/local/etc/tor/torrc
 
 #ORPORT for Relay
 read -p "Enter the port number you want ORPort to look at: " ORPort
-echo "ORPort $ORPort" >> /usr/local/etc/tor/torrc
 
 #DirPort for Relay
 read -p "Enter the port number you want DirPort to look at: " DirPort
-echo "DirPort $DirPort" >> /usr/local/etc/tor/torrc
+
 
 #Address for Relay
 read -p "Enter the IP address of the relay: " Address
-echo "Address $Address" >> /usr/local/etc/tor/torrc
+
 
 #Generate Authority Keys
-tor-gencert --create-identity-key -m 12 -a $Address:$DirPort \ -i /var/lib/tor/keys/authority_identity_key \ -s /var/lib/tor/keys/authority_signing_key \ -c /var/lib/tor/keys/authority_certificate
+tor-gencert --create-identity-key -m 12 -a $Address:$DirPort -i /var/lib/tor/keys/authority_identity_key -s /var/lib/tor/keys/authority_signing_key -c /var/lib/tor/keys/authority_certificate
 
 #Generate Router Keys
-tor --list-fingerprint --orport $ORPort \ --dirserver "x 127.0.0.1 ffffffffffffffffffffffffffffffffffffffff" \ --datadirectory /var/lib/tor/
+cd /var/lib/tor/keys/
+tor --list-fingerprint --orport $ORPort --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" --datadirectory /var/lib/tor/
 
 #Add DirAuthority lines to config file
-AuthCert="$(cat /var/lib/tor/keys/authority_certificate | grep fingerprint | grep -v "fingerprint ")"
-AuthFinger="$(cat /var/lib/tor/fingerprint | grep -v "$Name ")"
-echo "DirAuthority $Name orport=$ORPort v3ident= $AuthCert $Address:$DirPort $AuthFinger" >> /usr/local/etc/tor/torrc
+AuthCert="$(cat /var/lib/tor/keys/authority_certificate | grep 'fingerprint' | cut -f2- -d' ')"
+AuthFinger="$(cat /var/lib/tor/fingerprint | cut -f2- -d' ')"
+echo -e "DirAuthority $Name orport=$ORPort v3ident=$AuthCert $Address:$DirPort $AuthFinger" >> /usr/local/etc/tor/torrc
 
+#Set Configuration stuff
+echo >> /usr/local/etc/tor/torrc
+echo "SocksPort 0" >> /usr/local/etc/tor/torrc
+echo "ORPort $ORPort" >> /usr/local/etc/tor/torrc
+echo "DirPort $DirPort" >> /usr/local/etc/tor/torrc
+echo "Address $Address" >> /usr/local/etc/tor/torrc
+
+#Set Authority and Testing Parameters
+echo >> /usr/local/etc/tor/torrc
 echo "AuthoritativeDirectory 1" >> /usr/local/etc/tor/torrc
 echo "V3AuthoritativeDirectory 1" >>/usr/local/etc/tor/torrc
 echo "TestingV3AuthInitialVotingInterval 300 seconds" >> /usr/local/etc/tor/torrc
@@ -85,8 +97,10 @@ echo "TestingV3AuthInitialVoteDelay 20 seconds" >> /usr/local/etc/tor/torrc
 echo "TestingV3AuthInitialDistDelay 20 seconds" >> /usr/local/etc/tor/torrc
 
 #Exit policy for Relay
-echo "By default we do not allow exit policies for relays (this content is static.)"
-echo "Should this node be an exit node? (y,n)" REPLY
+echo "By default we do not allow exit policies for relays."
+read -p "Should this node be an exit node? (y,n)" REPLY
+
+echo "#Exit policy for IPv4 LAN" >> /usr/local/etc/tor/torrc
 
 if [ "${REPLY,,}" == "y" ]; then
 	
@@ -103,6 +117,4 @@ if [ "${REPLY,,}" == "n" ]; then
 	echo "ExitPolicy reject *:*" >> /usr/local/etc/tor/torrc
 fi
 
-#Contact info for Relay
-read -p "Enter your contact info for your relay: " Info
-echo "ContactInfo $Info" >> /usr/local/etc/tor/torrc
+#Contact i
